6b988011d368b827459c7e0a9b3061d375f05856
thomas rondio <sativva@free.fr>
2019-06-21 09:21:44 +0200

    remove test

--- test8/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ShopifyApp::AuthenticatedController
-  protect_from_forgery with: :exception
-  include Response
-  # before_action :authenticate_user!
-end
--- test8/app/jobs/products_create_job.rb
+++
@@ -1,8 +0,0 @@
-  class ProductsCreateJob < ApplicationJob
-    queue_as :default
-
-    def perform(*params)
-      p "____________CreateJob___________________"
-     # Do something later
-    end
-  end
